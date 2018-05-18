020beaa470e22ba71e95a6b611fe94a6a71a250e
ankitakalgut <kalgutkarankita@yahoo.in>
2018-05-18 10:58:49 +0530

    replacename

---
+++ ReplaceName.java
@@ -0,0 +1,35 @@
+package com.bridgelabz.FunctionalProgram;
+
+import com.bridgelabz.Utility.Utility;
+
+
+/**
+ * @author Ankita Kalgutkar
+ *
+ * 18-May-2018
+ * purpose:To replace the username with the given name.
+
+ */
+public class ReplaceName
+{
+
+	public static void main(String args[])
+	{
+		Utility test=new Utility();
+		String name=test.getString();
+		String template="Hello <<UserName>>,How are you?";
+		if(name.length()>=3)
+		{
+			
+		String res=template.replace("<<UserName>>",name);
+		System.out.println(res);
+		
+		}
+		else
+			System.out.println("invalid input");
+			
+		
+		
+		
+	}
+}
