2f08224d613ae7c44be91b6d56bc04b049b76c5f
ankitakalgut <kalgutkarankita@yahoo.in>
2018-05-25 17:21:24 +0530

    P

--- BubbleSort.java
+++ BubbleSort.java
@@ -2,50 +2,42 @@ package com.bridgelabz.Algorithmprograms;
 
 import com.bridgelabz.Utility.Utility;
 
-/*******************************************************************
+/*********************************************************************************************
  * @author Ankita Kalgutkar
  *
  * 18-May-2018
- * purpose:Bubble sort technique for integers.
+ * purpose:Bubble sort technique for integers and strings.
 
- *******************************************************************/
+ **********************************************************************************************/
 public class BubbleSort
 {  
-   static int n,i;
-   public static void main(String args[])
-   {	
+    static int n,i;
+    public static void main(String args[])
+    {	
 	    Utility test=new Utility();
-	
-
-	   int n=test.getInteger();
-	   /*
-	    int array[] = new int[n];   
+	    int n=test.getInteger();
+	    Integer array[] = new Integer [n];   
         for(int i = 0; i < n; i++)
         {
-
             array[i] = test.getInteger();
         }
-	   Utility.bubble(array);
-	   System.out.println("Array After Bubble Sort");  
-       for(int i=0; i < array.length; i++)
-       {  
-               System.out.print(array[i] + " ");  
-       }
-        */
+	    Utility.bubble(array);
+	    System.out.println("Array After Bubble Sort");  
+        for(int i=0; i < array.length; i++)
+        {  
+            System.out.print(array[i] + " ");  
+        }  
        //for string     
-       String[] array = new String[n];
+       String[] array1 = new String[n];
+       System.out.println();
        for (int i = 0; i < n;i++)
        {
-           array[i] = test.getString();
+           array1[i] = test.getString();
        } 
-     
-       Utility.bubble(array);
+       Utility.bubble(array1);
        for(int i=0; i < n; i++)
        {  
-               System.out.print(array[i] + " ");  
-       }
-       
-   }
-   
-   
+           System.out.print(array1[i] + " ");  
+       }   
+    }    
 }
