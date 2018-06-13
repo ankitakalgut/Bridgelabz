aeede8249cc1e9444fbaec8c7c676f5017f7df2b
ankitakalgut <kalgutkarankita@yahoo.in>
2018-06-13 17:55:04 +0530

    Deckofcards

--- DeckCards.java
+++ DeckCards.java
@@ -8,7 +8,7 @@ package com.bridgelabz.ObjectOriented;
  *Purpose:initialize deck of cards having suit ("Clubs","Diamonds", "Hearts", "Spades") & Rank ("2", "3", "4", "5",   
  *		  "6","7","8", "9", "10", "Jack",   "Queen",   "King",   "Ace").   Shuffle   the   cards   using   Random   method  
  *		   and then distribute 9 Cards to 4 Players and Print the Cards the received by the 4 Players  
-		   using 2D Array...
+		   using 2D Array..
  ********************************************************************************************************/
 public class DeckCards
 {
